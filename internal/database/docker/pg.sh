84ebe9867e223958223a66376c1ed3d22a6a71fa
Woodey <nikita_woodey@mail.ru>
2024-08-29 09:44:05 +0300

    change login psw and db_name to env vars in script

--- internal/database/docker/pg.sh
+++ internal/database/docker/pg.sh
@@ -1,8 +1,8 @@
 docker run -d --rm \
 	--name pgdb \
-	-e POSTGRES_PASSWORD=admin \
-	-e POSTGRES_USER=admin \
-	-e POSTGRES_DB=db \
+	-e POSTGRES_PASSWORD=POSTGRES_PASSWORD \
+	-e POSTGRES_USER=POSTGRES_USER \
+	-e POSTGRES_DB=POSTGRES_DB \
   -p 55000:5432 \
 	-e PGDATA=/var/lib/postgresql/data/pgdata \
 	-v .:/var/lib/postgresql/data \
