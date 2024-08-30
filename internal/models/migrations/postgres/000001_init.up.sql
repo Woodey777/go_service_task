2ab429b76fdc5ff7c131132e09009f800302a366
Woodey <nikita_woodey@mail.ru>
2024-08-30 12:01:31 +0300

    change int to serial in ids

--- internal/models/migrations/postgres/000001_init.up.sql
+++ internal/models/migrations/postgres/000001_init.up.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS projects 
 (
-  id INT NOT NULL,
+  id SERIAL NOT NULL,
   name CHAR(200) NOT NULL,
-  created_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT (now()),
   PRIMARY KEY (id)
 );
 CREATE INDEX IF NOT EXISTS projects_id ON projects(id);
-INSERT INTO projects VALUES (0, 'Первая запись', NOW());
+INSERT INTO projects VALUES (0, 'Первая запись');
 
 
 CREATE TABLE IF NOT EXISTS goods 
 (
-  id INT NOT NULL,
+  id SERIAL NOT NULL,
   project_id INT NOT NULL,
   name CHAR(200) NOT NULL,
   description CHAR(500),
   priority INT NOT NULL,
-  removed BOOLEAN NOT NULL,
-  created_at TIMESTAMP NOT NULL,
+  removed BOOLEAN NOT NULL DEFAULT false,
+  created_at TIMESTAMP NOT NULL DEFAULT (now()),
   PRIMARY KEY (id, project_id)
 );
 
