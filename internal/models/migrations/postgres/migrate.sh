5d22d75795b189f7fc58e384b98c98b7ee7d32a5
Woodey <nikita_woodey@mail.ru>
2024-08-30 11:59:59 +0300

    remove migrate.sh and add makefile with migrate targets

--- internal/models/migrations/postgres/migrate.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-migrate -path=$SCRIPT_DIR -database "postgresql://admin:admin@localhost:55000/db?sslmode=disable" -verbose up
