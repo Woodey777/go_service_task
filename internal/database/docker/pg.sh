0fe8ed561678701125a57ca8b586c82380fbf071
Woodey <nikita_woodey@mail.ru>
2024-08-30 12:00:53 +0300

    fix env vars in pg.sh

--- internal/database/docker/pg.sh
+++ internal/database/docker/pg.sh
@@ -1,9 +1,13 @@
+#bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 docker run -d --rm \
 	--name pgdb \
-	-e POSTGRES_PASSWORD=POSTGRES_PASSWORD \
-	-e POSTGRES_USER=POSTGRES_USER \
-	-e POSTGRES_DB=POSTGRES_DB \
+	-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+	-e POSTGRES_USER=$POSTGRES_USER \
+	-e POSTGRES_DB=$POSTGRES_DB \
   -p 55000:5432 \
 	-e PGDATA=/var/lib/postgresql/data/pgdata \
-	-v .:/var/lib/postgresql/data \
+	-v $SCRIPT_DIR:/var/lib/postgresql/data \
 	postgres
